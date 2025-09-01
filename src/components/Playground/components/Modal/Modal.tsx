aad11fe76fb09abc20c490a445fa133d88aea66a
Prosto2 <prosto20_2@mail.ru>
2025-09-01 13:24:44 +0300

    feat: реализация компонента ResultMessage

--- src/components/Playground/components/Modal/Modal.tsx
+++ src/components/Playground/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import type * as React from 'react';
 import { useAppDispatch } from '../../../../app/hooks.ts';
 import { resetStore } from '../../store/slices.ts';
+import ResultMessage from './components/ResultMessage';
 
 export interface IModalProps {
   setIsShowModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -15,11 +16,12 @@ const Modal: React.FC<IModalProps> = (props) => {
   const handleClose = () => {
     setIsShowModal(false);
     dispatch(resetStore());
-  }
+  };
 
   return (
     <div>
       <h3>Modal</h3>
+      <ResultMessage isSuccessEndGame={isSuccessEndGame} />
       <button onClick={handleClose}>Start New Game</button>
     </div>
   );
--- src/components/Playground/store/slices.ts
+++ src/components/Playground/store/slices.ts
@@ -66,10 +66,15 @@ export const playgroundSlice = createSlice({
       }
     },
 
-    resetStore: () => initialState,
+    resetStore: () => initialState
   }
 });
 
-export const { setCurrentStep, setSteps, setEnteredValue, setUnsuccess, resetStore} =
-  playgroundSlice.actions;
+export const {
+  setCurrentStep,
+  setSteps,
+  setEnteredValue,
+  setUnsuccess,
+  resetStore
+} = playgroundSlice.actions;
 export default playgroundSlice.reducer;
