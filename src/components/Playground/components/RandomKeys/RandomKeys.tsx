33ccb9edf26262eac96b32a0496b777388081667
Prosto2 <prosto20_2@mail.ru>
2025-09-01 13:44:57 +0300

    feat: реализация стилей для стрелок компонента RandomKeys

--- src/components/Playground/components/RandomKeys/RandomKeys.tsx
+++ src/components/Playground/components/RandomKeys/RandomKeys.tsx
@@ -2,6 +2,9 @@ import type * as React from 'react';
 import { useAppSelector } from '../../../../app/hooks.ts';
 import { MAP_ARROW_CODES } from '../../constants.ts';
 import type { IMapArrowCodes } from '../../types.ts';
+import type { IPlaygroundStepsState } from '../../store/types.ts';
+
+import styles from "./RandomKeys.module.css";
 
 export type IRandomKeysProps = {
   isTimerActive: boolean;
@@ -12,11 +15,21 @@ const RandomKeys: React.FC<IRandomKeysProps> = () => {
 
   const state = useAppSelector((state) => state.playground);
 
+  const getStylesRandomKeys = (element: IPlaygroundStepsState): string => {
+    if(element.success) {
+      return styles.iconSuccess;
+    } else if (element.success !== null) {
+      return styles.iconUnSuccess;
+    }
+
+    return styles.icon;
+  }
+
   console.log(state.steps);
   return (
     <div>
       {state.steps.map((element) => (
-        <span key={element.step}>
+        <span key={element.step} className={getStylesRandomKeys(element)}>
           {MAP_ARROW_CODES[element.currentValue as keyof IMapArrowCodes]}
         </span>
       ))}
