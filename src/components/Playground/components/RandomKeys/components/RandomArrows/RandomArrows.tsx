b998010d15448a863ec61c20160374d3c37cc4db
Prosto2 <prosto20_2@mail.ru>
2025-09-04 14:17:52 +0300

    feat: стилизация компонента RandomKeys, добавление loader и favicon

--- src/components/Playground/components/RandomKeys/components/RandomArrows/RandomArrows.tsx
+++ src/components/Playground/components/RandomKeys/components/RandomArrows/RandomArrows.tsx
@@ -1,31 +1,34 @@
+import cn from 'classnames';
 import type * as React from 'react';
 import { MAP_ARROW_CODES } from '../../../../constants.ts';
 import type { IMapArrowCodes } from '../../../../types.ts';
 import type { IPlaygroundStepsState } from '../../../../store/types.ts';
-import styles from './RandomArrows.module.css';
 import { useAppSelector } from '../../../../../../app/hooks.ts';
+
+import stylesCommon from '../../RandomKeys.module.css';
+import styles from './RandomArrows.module.css';
 
 const RandomArrows: React.FC = () => {
   const state = useAppSelector((state) => state.playground);
 
   const getStylesRandomKeys = (element: IPlaygroundStepsState): string => {
     if (element.success) {
-      return styles.iconSuccess;
+      return cn(stylesCommon.icon, styles.iconSuccess);
     } else if (element.success !== null) {
-      return styles.iconUnSuccess;
+      return cn(stylesCommon.icon, styles.iconUnSuccess);
     }
 
-    return styles.icon;
+    return stylesCommon.icon;
   };
 
   return (
-    <>
+    <div className={stylesCommon.wrapper}>
       {state.steps.map((element) => (
         <span key={element.step} className={getStylesRandomKeys(element)}>
           {MAP_ARROW_CODES[element.currentValue as keyof IMapArrowCodes]}
         </span>
       ))}
-    </>
+    </div>
   );
 };
 
--- src/components/Playground/components/RandomKeys/components/WelcomeText/WelcomeText.tsx
+++ src/components/Playground/components/RandomKeys/components/WelcomeText/WelcomeText.tsx
@@ -1,5 +1,9 @@
 import type * as React from 'react';
 import { TypographyText } from '../../../../../UI';
+import loader from './img/loader.svg';
+
+import stylesCommon from '../../RandomKeys.module.css';
+import styles from './WelcomeText.module.css';
 
 export type IWelcomeText = {
   isTimerActive: boolean;
@@ -9,7 +13,13 @@ export const WelcomeText: React.FC<IWelcomeText> = (props) => {
   const { isTimerActive } = props;
 
   if (isTimerActive) {
-    return <span>Loading...</span>;
+    return (
+      <div className={stylesCommon.wrapper}>
+        <span className={stylesCommon.icon}>
+          <img className={styles.loader} src={loader} alt='loader' />
+        </span>
+      </div>
+    );
   }
 
   return <TypographyText>Press "Play" to start the game</TypographyText>;
