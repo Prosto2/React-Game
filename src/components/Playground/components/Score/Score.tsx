318326a0f213e25eca507c87bb05bb6ad378e63d
Prosto2 <prosto20_2@mail.ru>
2025-09-04 12:37:34 +0300

    feat: стилизация компонента Score

--- src/components/Playground/components/Score/Score.tsx
+++ src/components/Playground/components/Score/Score.tsx
@@ -1,6 +1,9 @@
 import type * as React from 'react';
 import { useAppSelector } from '../../../../app/hooks.ts';
 import { TypographyHeader, TypographyText } from '../../../UI';
+import { Chip, Stack } from '@mui/material';
+
+import styles from './Score.module.css';
 
 const Score: React.FC = () => {
   const state = useAppSelector((state) => state.playground);
@@ -11,9 +14,29 @@ const Score: React.FC = () => {
       <TypographyText>
         On error, the "Consecutive successful hits" value is reset to zero
       </TypographyText>
-      <span>Errors: {state.totalUnsuccessful}</span>
-      <br />
-      <span>Successful: {state.totalSuccessful}</span>
+
+      <Stack direction='row' spacing={1}>
+        <Chip
+          className={styles.chipUnsuccess}
+          variant='outlined'
+          label={
+            <>
+              Errors:{' '}
+              <span className={styles.counter}>{state.totalUnsuccessful}</span>
+            </>
+          }
+        />
+        <Chip
+          className={styles.chipSuccessful}
+          variant='outlined'
+          label={
+            <>
+              Successful:{' '}
+              <span className={styles.counter}>{state.totalSuccessful}</span>
+            </>
+          }
+        />
+      </Stack>
     </>
   );
 };
